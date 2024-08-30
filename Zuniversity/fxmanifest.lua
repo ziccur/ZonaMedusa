97154f6d1dc0dfbfebb171c530dfdcfc41673b2f
Yeray Reyes <yerayreyes@protonmail.com>
2024-08-30 21:00:33 +0200

    manifest created

---
+++ Zuniversity/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Ziccur University'
+lua54 'yes'
+
+version '1.0' 
+legacyversion '1.9.1'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua'
+}
+
+server_exports {
+	'GetUniversitieHabilies'
+}
+
