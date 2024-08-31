91e7a1eb6d40691752176f38d3797ca3de509cf6
Yeray Reyes <yerayreyes@protonmail.com>
2024-08-31 16:26:50 +0200

    El menu no se abre cuando lo cierrar y comprueba si tienes saldo disponible

--- Zuniversity/client.lua
+++ Zuniversity/client.lua
@@ -1,8 +1,9 @@
 ESX = exports["es_extended"]:getSharedObject()
 local menuIsShowed = false
+local menuIsClosed = false
 
-local data = Config.dataMenu
 local location = Config.location
+local options = Config.options
 
 Size = {
     x = 3.0,
@@ -17,8 +18,43 @@ CreateThread(function()
         
         --? Comprueba si el jugador esta en el rango del punto
         if (#(coords - vector3(location.x, location.y, location.z)) < Size.x ) then
-            if not menuIsShowed then
-                ESX.UI.Menu.Open("default", "Zuniversity", "Universidad", data, submit, cancel, change, close)
+            if not menuIsShowed and not menuIsClosed then
+                ESX.UI.Menu.Open("default", "Zuniversity", "Universidad", Config.dataMenu, 
+
+                function(data, menu)
+                    local selectedOption = data.current.value
+                    for i = 1, #options do
+                        if options[i].value == selectedOption then
+                            selectedOption = options[i]
+                        end
+                    end
+
+                    while ESX.GetPlayerData().accounts == nil do
+                        Citizen.Wait(10)
+                    end
+                
+                    -- Acceder al saldo bancario
+                    local avaliableMoney = nil
+                    for _, account in ipairs(ESX.GetPlayerData().accounts) do
+                        if account.name == Config.payMethod then
+                            avaliableMoney = account
+                            break
+                        end
+                    end
+
+                    if selectedOption.money > 0 then
+                        if avaliableMoney.money < selectedOption.money then -- Player don't have enough money
+                            ESX.ShowNotification("No tienes suficiente dinero")
+                        else
+                            ESX.ShowNotification("Has pagado " .. selectedOption.money .. "$")
+                        end
+                    end
+
+                    menu.close()
+                    menuIsShowed = false
+                    menuIsClosed = true
+                end, 
+                cancel, change, close)
                 menuIsShowed = true
             end
            
@@ -26,6 +62,7 @@ CreateThread(function()
             while menuIsShowed and not (#(coords - vector3(location.x, location.y, location.z)) < Size.x ) do 
                 ESX.UI.Menu.Close("default", "Zuniversity", "Universidad")
                 menuIsShowed = false
+                menuIsClosed = false
                 Wait(0)
             end
 
@@ -33,10 +70,13 @@ CreateThread(function()
         else --? si el jugador no esta en el rango elimina el menu
             ESX.UI.Menu.Close("default", "Zuniversity", "Universidad")
             menuIsShowed = false
+            menuIsClosed = false
         end
         Wait(500)
     end
 end)
+
+-- Función que se ejecuta cuando el usuario selecciona una opción en el menú
 
 
   
--- Zuniversity/config.lua
+++ Zuniversity/config.lua
@@ -1,21 +1,17 @@
 Config= {}
 
-
-Config.location = vector3(-1637.1189, 180.4041, 61.7573)
-Config.payMethod = "bank" -- bank or cash
-Config.spawnPed = true
-Config.pedModel = "s_m_y_dealer_01"
+Config.location = vector3(-1637.1189, 180.4041, 61.7573) -- location of the point, ped, etc
+Config.payMethod = "money" -- bank or money
+Config.spawnPed = true -- true or false
+Config.pedModel = "s_m_y_dealer_01" -- ped model https://wiki.rage.mp/index.php?title=Peds
 
 Config.options = { 
-    {label = "Dinero x2  -  100.000$", value = "opcion1", money = 0, levels = 0, items = 0},
-    {label = "Opción 2", value = "opcion2", money = 0, levels = 0, items = 0}
+    {label = "Dinero x2  -  100.000$", value = "option1", money = 100000, levels = 0, items = 0},
+    {label = "Opción 2", value = "option2", money = 0, levels = 0, items = 0}
  }
  Config.dataMenu = {
-    title = "Mi Menú",
-    align = "top-left",
-    elements = {}
+    title = "Mi Menú", -- menu title
+    align = "top-left", -- menu position
+    elements = {} -- don't touch
 }
-
-for i = 1, #Config.options do
-    Config.dataMenu.elements[i] = {label = Config.options[i].label, value = Config.options[i].value}
-end
+for i = 1, #Config.options do Config.dataMenu.elements[i] = {label = Config.options[i].label, value = Config.options[i].value} end
