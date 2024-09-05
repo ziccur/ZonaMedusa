9a433e998ab581a3a84710005c6cb921b7a7b58d
Yeray Reyes <yerayreyes@protonmail.com>
2024-09-06 01:43:12 +0200

    Refactoring of config, all inside Config
    Co-Authored-By: Yxel14 <168170086+Yxelixx14@users.noreply.github.com>
    Co-Authored-By: jitodv <113702662+jitodv@users.noreply.github.com>

--- config.lua
+++ config.lua
@@ -1,51 +1,53 @@
 
 -- All configs explained in the README.md file
 
-Config= {}
+Config= {
 
-Config.Lenguage = 'en'
-Config.location = vector3(1748.0743, 3268.5168, 41.2383)
+    Locale = GetConvar('esx:locale', 'es'), --? https://docs.fivem.net/docs/scripting-reference/convars/
 
-Config.allowedJobs = {
-    'police',
-    'ambulance'
+    location = vector3(1748.0743, 3268.5168, 41.2383),
+
+    allowedJobs = {
+        'police',
+        'ambulance'
+    },
+
+    allowedIllegalJobs = {
+        'mechanic'
+    },
+
+    NPCspawn = false,
+    pedModel = 's_m_y_cop_01', --? https://docs.fivem.net/docs/game-references/ped-models/
+
+    conquerKey = 38, --? https://docs.fivem.net/docs/game-references/controls/
+
+    Size = { 
+        x = 100,
+        y = 100,
+        z = 100
+    },
+
+    timeToConquer = 10, --? Time to conquer the zone (seconds)
+    timeToReward = 5, --? Time to reward the zone (seconds)
+
+    ilegalReward = {
+        cocaine_bag = 1,
+        weed_bag = 1,
+        meth_bag = 1
+    },
+
+    legalReward = {
+        --? You can use "money", "bank" or item name as reward
+        money = 1000,
+        bank = 1000,
+        diamond = 1
+    },
+
+    BlipColour = 1, 
+    BlipSprite = 429, -- https://docs.fivem.net/docs/game-references/blips/
+    BlipAlpha = 50, 
+    BlipRadius = 100.0, 
+
+    defaultOwner = 'Nobody'
+
 }
-
-Config.allowedIllegalJobs = {
-    'mechanic'
-}
-
-Config.NPCspawn = false
-Config.pedModel = 's_m_y_cop_01'--? https://docs.fivem.net/docs/game-references/ped-models/
-
-Config.conquerKey = 38 --? https://docs.fivem.net/docs/game-references/controls/
-
-Config.Size = { 
-    x = 100,
-    y = 100,
-    z = 100
-}
-
-Config.timeToConquer = 10 --? Time to conquer the zone (seconds)
-Config.timeToReward = 5 --? Time to reward the zone (seconds)
-
-Config.ilegalReward = {
-    cocaine_bag = 1,
-    weed_bag = 1,
-    meth_bag = 1
-}
-
-Config.legalReward = {
-    --? You can use "money", "bank" or item name as reward
-    money = 1000,
-    bank = 1000,
-    diamond = 1
-}
-
-Config.BlipColour = 1 
-Config.BlipSprite = 429 -- https://docs.fivem.net/docs/game-references/blips/
-Config.BlipAlpha = 50 
-Config.BlipRadius = 100.0 
-
-Config.defaultOwner = 'Nobody'
-
